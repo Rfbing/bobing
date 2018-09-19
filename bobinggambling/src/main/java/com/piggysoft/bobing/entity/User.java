53133be8348d522105d685e12539714dc46ecf58
john2 <john2@john2-PC>
2018-09-19 10:14:13 +0800

    add CRUD of user table Signed-off-by: abing

--- bobinggambling/src/main/java/com/piggysoft/bobing/entity/User.java
+++ bobinggambling/src/main/java/com/piggysoft/bobing/entity/User.java
@@ -2,6 +2,8 @@ package com.piggysoft.bobing.entity;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -10,6 +12,7 @@ import javax.persistence.Table;
 public class User {
 	 private static final long serialVersionUID = 1L;
 
+	 	@GeneratedValue(strategy = GenerationType.AUTO)
 	    @Id
 	    @Column(name="user_id")
 	    int userId;
--- bobinggambling/src/main/java/com/piggysoft/bobing/jpa/UserRepository.java
+++ bobinggambling/src/main/java/com/piggysoft/bobing/jpa/UserRepository.java
@@ -6,5 +6,21 @@ import com.piggysoft.bobing.entity.User;
 
 public interface UserRepository extends PagingAndSortingRepository<User, Integer> {
 	// List<User> findAllByOrderByScoresAtDesc();
-	
+	User findByNickName(String nickName);
+//	@Modifying(clearAutomatically = true) 
+//	@Query("update User a set a.scores =:scores where a.nickName =:nickName")  
+//	public void updateScoresAndDes(@Param("scores") int scores,
+//			@Param("bobingDescriptions") String bobingDescriptions,
+//			@Param("nickName")String nickName)
+//	
+//	@Modifying(clearAutomatically = true) 
+//	@Query("update User a set a.scores =:scores where a.nickName =:nickName")  
+//	public void updateBoBingTimes(@Param("haveBobingTimes") int haveBobingTimes,
+//			@Param("usedBobingTimes") int usedBobingTimes,
+//			@Param("nickName")String nickName)
+//	
+//	
+//	@Modifying(clearAutomatically = true) 
+//	@Query("update User a set a.scores =:scores where a.nickName =:nickName")  
+//	public void updateScores(@Param("scores") int scores,@Param("nickName")String nickName)
 }
