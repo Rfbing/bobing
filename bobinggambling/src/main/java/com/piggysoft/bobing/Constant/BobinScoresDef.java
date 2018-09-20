b6333bb7175eb037430487fc37b400839c9f5dd7
john2 <john2@john2-PC>
2018-09-20 16:38:25 +0800

    add two file
    Signed-off-by: abing

---
+++ bobinggambling/src/main/java/com/piggysoft/bobing/Constant/BobinScoresDef.java
@@ -0,0 +1,20 @@
+package com.piggysoft.bobing.Constant;
+
+public class BobinScoresDef {
+
+	public static int FOUR_FOUR_TWO_ONE = 1800;//状元插金花
+	public static int ALL_FOUR = 1800;//六勃红
+	public static int ALL_ONE = 1500;//遍地锦
+	public static int BLACK_SIX = 1200;//六勃黑	
+	public static int FIVE_FOUR = 1000;//五红	
+	public static int FIVE_STAGE = 800;//五子
+	public static int FOUR_FOUR = 700;//四红
+	public static int ONE_TO_SIX = 700;//对堂
+	public static int THREE_FOUR = 400;//三红
+	public static int FOUR_OR = 400;//四进
+	public static int TWO_FOUR = 200;//二举
+	
+	public static int ONE_FOUR = 100;//一秀
+	public static int NOTHING = 0;//什么也没有
+	
+}
